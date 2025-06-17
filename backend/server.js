b39aa52aed652a539f9aed975f3c54cd902db58c
Amir <amiraglodiya01@gmail.com>
2025-06-17 17:36:38 +0530

    Clean project with updated .gitignore

--- backend/server.js
+++ backend/server.js
@@ -33,5 +33,5 @@ app.post("/contact", async (req, res) => {
 });
 
 // Server start
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server is running on port 5000"));
